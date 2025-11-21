82bdce641c2f2db40343e85485d35612ee98c88f
prasad20001205 <prasadsamaranayake184@gmail.com>
2025-11-21 12:21:53 +0530

    Update package.json to reflect correct entry point and add server implementation in src/index.js; create User model.

---
+++ server/src/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Simple route
+app.get('/', (req, res) => res.send('Peer Hub API'));
+
+// Server & Socket.io
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: process.env.CLIENT_URL }
+});
+io.on('connection', socket => {
+  console.log('New socket connection:', socket.id);
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
